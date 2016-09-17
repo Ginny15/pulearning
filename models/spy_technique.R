3dca4bc653c489f1bf3aa8e439fb285dfbef8e3c
Ginny15 <xue.hu.15@ucl.ac.uk>
2016-09-17 15:55:53 +0100

    deleting some content against NDA

--- models/spy_technique.R
+++ models/spy_technique.R
@@ -69,40 +69,5 @@ metric = prob.r^2/prob.fx.1 #7.04
 kmeans()
 write.csv(cbind(test,data.frame(test,pred=predict(fit.xgb,test[,-1]),prob = predict(fit.xgb,test[,-1],type = 'prob'))),'results_all_catego.0817.testset.csv')
 
-#### one class svm
-suppressWarnings(suppressMessages(library(oneClass)))
-suppressWarnings(suppressMessages(library(doParallel)))
-suppressWarnings(suppressMessages(library(raster)))
-suppressWarnings(suppressMessages(library(RColorBrewer)))
 
-cl <- makeCluster(detectCores()-1)
-doParallel:::registerDoParallel(cl)
-
-data.x = dummy.data.frame(data[,c(2:9,11)])
-data.y = data$label
-data = cbind(data.x,data.y)
-trainIDX = sample(nrow(data),nrow(data)*0.75,replace = FALSE)
-train = data[trainIDX,];test = data[-trainIDX,]
-train.pos = train[which(train$data.y=='Y'),] #2652
-train.un = train[-which(train$data.y=='Y'),]  #67413
-train.un.subset = train.un[sample(nrow(train.un),5000,replace = FALSE),]
-train = rbind(train.pos,train.un.subset)
-train = train[shuffle(nrow(train)),]
-library(oneClass)
-seed <- 123456
-train.x <- train[,-215];
-train.y <- puFactor(train[,215], positive='Y')
-set.seed(seed)
-train.index <- createFolds(train.y, k=3, returnTrain=TRUE)
-set.seed(seed)
-test.x <- test[,-215]
-test.y <- puFactor(test[,215], positive='Y')
-sigma = 0.1, cNeg = 4 and cMultiplier = 16384
-tuneGrid <- expand.grid( sigma =seq(0.01, 0.1), cNeg = seq(1, 2, 4) ,cMultiplier=2056)
-bsvm.fit <- trainOcc(x=train.x, y=train.y, index=train.index,tuneGrid=tuneGrid)
-bsvm.fit.def <- bsvm.fit # store for comparison
-bsvm.pred <- predict(bsvm.fit, test.x)
-bsvm.bin <- bsvm.pred>0
-bsvm.bin = puFactor(bsvm.bin, positive='TRUE')
-confusionMatrix(bsvm.bin,test.y)$table
  
